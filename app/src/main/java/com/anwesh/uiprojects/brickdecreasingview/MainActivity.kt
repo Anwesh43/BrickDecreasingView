4c8095d5fca33ad1f9b205037d2187678fced1e3
Anwesh43 <anweshthecool0@gmail.com>
2018-07-20 01:25:09 +0530

    adding BDView in Activity

--- app/src/main/java/com/anwesh/uiprojects/brickdecreasingview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/brickdecreasingview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.brickdecreasingview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bdview.BDView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BDView.create(this)
     }
 }
