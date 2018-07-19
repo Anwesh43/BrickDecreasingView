f49a21d5182f63135faa391dad17a1ef47065d5b
Anwesh43 <anweshthecool0@gmail.com>
2018-07-20 00:48:45 +0530

    adding bdview class

--- bdview/src/main/java/com/anwesh/uiprojects/bdview/BDView.kt
+++ bdview/src/main/java/com/anwesh/uiprojects/bdview/BDView.kt
@@ -8,4 +8,24 @@ import android.view.View
 import android.view.MotionEvent
 import android.content.Context
 import android.graphics.Paint
-import android.graphics.Canvas 
+import android.graphics.Canvas
+
+val nodes : Int = 5
+
+class BDView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
