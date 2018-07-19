58a979c482f47877a533293d589366fe1bcb4513
Anwesh43 <anweshthecool0@gmail.com>
2018-07-20 00:41:09 +0530

    adding BDView class

---
+++ bdview/src/main/java/com/anwesh/uiprojects/bdview/BDView.kt
@@ -0,0 +1,11 @@
+package com.anwesh.uiprojects.bdview
+
+/**
+ * Created by anweshmishra on 20/07/18.
+ */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas 
